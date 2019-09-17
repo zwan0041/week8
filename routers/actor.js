412799a3b2d1ac5604964cc067a92e9102d8de88
zijianwang <zwan0041@student.monash.edu>
2019-09-18 00:44:43 +1000

    initial commit

---
+++ routers/actor.js
@@ -0,0 +1,115 @@
+const mongoose = require('mongoose');
+
+const Actor = require('../models/actor');
+const Movie = require('../models/movie');
+
+module.exports = {
+
+    // getAll: function (req, res) {
+    //     Actor.find(function (err, actors) {
+    //         if (err) {
+    //             return res.status(404).json(err);
+    //         } else {
+    //             res.json(actors);
+    //         }
+    //     });
+    // },
+
+    getAll: function (req, res) {
+        Actor.find().populate('movies',['title','year']).exec(function (err, actors) {
+            if (err) {
+                return res.status(404).json(err);
+            } else {
+                res.json(actors);
+            }
+        });
+    },
+
+    createOne: function (req, res) {
+        let newActorDetails = req.body;
+        newActorDetails._id = new mongoose.Types.ObjectId();
+
+        let actor = new Actor(newActorDetails);
+        actor.save(function (err) {
+            res.json(actor);
+        });
+    },
+
+    getOne: function (req, res) {
+        Actor.findOne({ _id: req.params.id })
+            .populate('movies')
+            .exec(function (err, actor) {
+                if (err) return res.status(400).json(err);
+                if (!actor) return res.status(404).json();
+                res.json(actor);
+            });
+    },
+
+
+    updateOne: function (req, res) {
+        Actor.findOneAndUpdate({ _id: req.params.id }, req.body, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+
+            res.json(actor);
+        });
+    },
+
+
+    deleteOne: function (req, res) {
+        Actor.findOneAndRemove({ _id: req.params.id }, function (err) {
+            if (err) return res.status(400).json(err);
+
+            res.json();
+        });
+    },
+
+
+    addMovie: function (req, res) {
+        Actor.findOne({ _id: req.params.id }, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+
+            Movie.findOne({ _id: req.body.id }, function (err, movie) {
+                if (err) return res.status(400).json(err);
+                if (!movie) return res.status(404).json();
+
+                actor.movies.push(movie._id);
+                actor.save(function (err) {
+                    if (err) return res.status(500).json(err);
+
+                    res.json(actor);
+                });
+            })
+        });
+    },
+
+    deleteOneWithMovie:function(req,res){
+        Movie.deleteMany({actors:req.params.id},function(err){
+            if (err) return res.status(400).json(err);
+            Actor.findOneAndRemove({ _id: req.params.id }, function (err) {
+                if (err) return res.status(400).json(err);
+                res.json();
+            });
+        })
+    },
+
+    deleteMovie: function (req, res) {
+        Actor.findOneAndUpdate({ _id: req.params.actorId }, {$pull:{movies:req.params.movieId}}, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+
+            res.json(actor);
+        });
+    },
+
+    deleteYoung:function(req,res){
+        var currentTime = new Date();
+        year=currentTime.getFullYear()
+        Actor.deleteMany({bYear:{$gte:year-15}},function(err){
+            if (err) return res.status(400).json(err);
+            res.json();
+        })
+    }
+
+};
